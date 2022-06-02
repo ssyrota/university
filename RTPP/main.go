10127874f5b62b32c3104ae27aa01a7274ca232e
sergeycrisp <100839979+sszenbit@users.noreply.github.com>
2022-06-03 00:29:57 +0300

    app Mpig

--- RTPP/main.go
+++ RTPP/main.go
@@ -37,7 +37,7 @@ func mainProcess(worldComm *mpi.Communicator) {
 	worldSize := worldComm.Size()
 	fmt.Printf("Processors: %d\n", worldSize)
 	// Form sorted points
-	points := qh.EnumPossiblePoints(GeneratePoints(5000000))
+	points := qh.EnumPossiblePoints(GeneratePoints(5000))
 
 	start := time.Now()
 
@@ -52,7 +52,7 @@ func mainProcess(worldComm *mpi.Communicator) {
 	s2 := qh.PointsAtLeftSide(maxRight, maxLeft, points)
 
 	// Track used processes
-	usedProcesses := distributedQuickHull(maxLeft, maxRight, s1, s2, worldComm)
+	usedProcesses, performedPoints := distributedQuickHull(maxLeft, maxRight, s1, s2, worldComm)
 
 	var resultsSync []qh.Point
 	if worldSize == 2 {
@@ -65,48 +65,68 @@ func mainProcess(worldComm *mpi.Communicator) {
 			pts := qh.ReceivePoints(worldComm, pId, qh.MainAlgorithmCh)
 			resultsSync = append(resultsSync, pts...)
 		}
-
 	}
 	// Form result
 	pie.SortStableUsing(
 		pie.Unique(
 			qh.ConcatList(
-				[]qh.Point{maxLeft, maxRight},
+				append([]qh.Point{maxLeft, maxRight}, performedPoints...),
 				resultsSync)),
 		qh.CompareByName)
 
 	elapsed := time.Since(start)
 	fmt.Printf("Performed time: %v ms\n", elapsed.Milliseconds())
-	// fmt.Printf("Master received result: %v\n\n", res)
 }
 
-func distributedQuickHull(maxLeft, maxRight qh.Point, s1, s2 []qh.Point, worldComm *mpi.Communicator) []int {
+func distributedQuickHull(maxLeft, maxRight qh.Point, s1, s2 []qh.Point, worldComm *mpi.Communicator) ([]int, []qh.Point) {
 	// Count processes
 	worldSize := worldComm.Size()
 	var processes []int
 	for pId := 1; pId < worldSize; pId++ {
 		processes = append(processes, pId)
 	}
+	// 1 worker
 	if len(processes) == 1 {
 		qh.SendPoints(qh.ConcatList([]qh.Point{maxLeft, maxRight}, s1), worldComm, processes[0], qh.MainAlgorithmCh)
 		qh.SendPoints(qh.ConcatList([]qh.Point{maxRight, maxLeft}, s2), worldComm, processes[0], qh.MainAlgorithmCh)
-		return processes
+		return processes, []qh.Point{}
 	}
 
+	// 2 workers
 	if len(processes) == 2 {
 		qh.SendPoints(qh.ConcatList([]qh.Point{maxLeft, maxRight}, s1), worldComm, processes[0], qh.MainAlgorithmCh)
 		qh.SendPoints(qh.ConcatList([]qh.Point{maxRight, maxLeft}, s2), worldComm, processes[1], qh.MainAlgorithmCh)
-		return processes
+		return processes, []qh.Point{}
 	}
 
+	// 3 workers
 	if len(processes) == 3 {
-		qh.SendPoints(qh.ConcatList([]qh.Point{maxLeft, maxRight}, s1), worldComm, processes[0], qh.MainAlgorithmCh)
-		qh.SendPoints(qh.ConcatList([]qh.Point{maxRight, maxLeft}, s2), worldComm, processes[1], qh.MainAlgorithmCh)
-		return processes
+		h, s1part1, s1part2 := qh.QuickHullSeparator(maxLeft, maxRight, s1)
+		qh.SendPoints(qh.ConcatList([]qh.Point{maxLeft, h}, s1part1), worldComm, processes[0], qh.MainAlgorithmCh)
+		qh.SendPoints(qh.ConcatList([]qh.Point{h, maxRight}, s1part2), worldComm, processes[1], qh.MainAlgorithmCh)
+		qh.SendPoints(qh.ConcatList([]qh.Point{maxLeft, maxRight}, s2), worldComm, processes[2], qh.MainAlgorithmCh)
+		return processes, []qh.Point{h}
 	}
 
-	return processes
+	panic("not-implemented-scalability")
 
+}
+
+// Second and more recursive steps of an algorithm
+func QuickHullHelperMpi(a, b qh.Point, points []qh.Point) []qh.Point {
+	// Case than triangle variant is one
+	if len(points) <= 1 {
+		return points
+	}
+
+	h, s1, s2 := qh.QuickHullSeparator(a, b, points)
+
+	// Run recursive steps
+	leftHull := QuickHullHelperMpi(a, h, s1)
+	rightHull := QuickHullHelperMpi(h, b, s2)
+
+	res := qh.ConcatList(rightHull, leftHull, []qh.Point{h})
+	return res
 }
 
 // Sub process
@@ -136,22 +156,5 @@ func GeneratePoints(n int32) []qh.Point {
 	for i = 0; i < n; i++ {
 		res[i] = qh.Point{X: rand.Float64() * float64(n), Y: rand.Float64() * float64(n), Name: i}
 	}
-	return res
-}
-
-// Second and more recursive steps of an algorithm
-func QuickHullHelperMpi(a, b qh.Point, points []qh.Point) []qh.Point {
-	// Case than triangle variant is one
-	if len(points) <= 1 {
-		return points
-	}
-
-	h, s1, s2 := qh.QuickHullSeparator(a, b, points)
-
-	// Run recursive steps
-	leftHull := QuickHullHelperMpi(a, h, s1)
-	rightHull := QuickHullHelperMpi(h, b, s2)
-
-	res := qh.ConcatList(rightHull, leftHull, []qh.Point{h})
 	return res
 }
