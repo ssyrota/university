b2b8639c26f3bf5a1b41887749358a242465a6cb
Serhii <sergey.crisp@gmail.com>
2022-10-30 13:11:59 +0200

    separate load file component

--- mobile_dev/project_1/app/src/main/java/com/example/csc_knu_mobile_development_project_1/core/InputFileList.kt
+++ mobile_dev/project_1/app/src/main/java/com/example/csc_knu_mobile_development_project_1/core/InputFileList.kt
@@ -1,2 +1,45 @@
 package com.example.csc_knu_mobile_development_project_1.core
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.double
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+@Composable
+fun LoadFile(inputByFileClick: (l: List<Double>) -> Unit) {
+	val context = LocalContext.current
+	val contentResolver = context.contentResolver
+	val launcher =
+		rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
+			uri?.let {
+				val content = BufferedReader(
+					InputStreamReader(
+						contentResolver.openInputStream(
+							it
+						)
+					)
+				).readText()
+				inputByFileClick(
+					jsonToListDouble(
+						content
+					)
+				)
+			}
+		}
+	MainButton("Load from file") {
+		launcher.launch("application/json")
+	}
+}
+
+fun jsonToListDouble(t: String): List<Double> {
+	return Json.parseToJsonElement(t).jsonArray.map {
+		it.jsonPrimitive.double
+	}
+}
--- mobile_dev/project_1/app/src/main/java/com/example/csc_knu_mobile_development_project_1/core/MainPage.kt
+++ mobile_dev/project_1/app/src/main/java/com/example/csc_knu_mobile_development_project_1/core/MainPage.kt
@@ -1,7 +1,5 @@
 package com.example.csc_knu_mobile_development_project_1.core
 
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -11,15 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.double
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonPrimitive
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
 @Composable
 fun MainPage(inputByHandClick: () -> Unit, loadFromFileClick: (l: List<Double>) -> Unit) {
@@ -57,37 +48,5 @@ fun MainButton(text: String, callback: () -> Unit) {
 			text = text, style = MaterialTheme.typography.button,
 			fontSize = 25.sp
 		)
-	}
-}
-
-@Composable
-fun LoadFile(inputByFileClick: (l: List<Double>) -> Unit) {
-	val context = LocalContext.current
-	val contentResolver = context.contentResolver
-	val launcher =
-		rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
-			uri?.let {
-				val content = BufferedReader(
-					InputStreamReader(
-						contentResolver.openInputStream(
-							it
-						)
-					)
-				).readText()
-				inputByFileClick(
-					jsonToListDouble(
-						content
-					)
-				)
-			}
-		}
-	MainButton("Load from file") {
-		launcher.launch("application/json")
-	}
-}
-
-fun jsonToListDouble(t: String): List<Double> {
-	return Json.parseToJsonElement(t).jsonArray.map {
-		it.jsonPrimitive.double
 	}
 }
