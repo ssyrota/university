fb0b165c95a65dd65302a0ad1783b034de0732d5
Serhii <sergey.crisp@gmail.com>
2022-11-04 12:39:03 +0200

    add course_work

--- mobile_dev/project_2/app/src/main/java/com/example/project_2/MainActivity.kt
+++ mobile_dev/project_2/app/src/main/java/com/example/project_2/MainActivity.kt
@@ -1,43 +1,137 @@
 package com.example.project_2
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.project_2.core.ContactsEndsWith
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.project_2.ui.theme.Project_2Theme
 
+
 class MainActivity : ComponentActivity() {
+    var contactName by mutableStateOf("")
+    var contactNumber by mutableStateOf("")
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Project_2Theme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    ContactsEndsWith()
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(backgroundColor = Color.Green,
+                                title = {
+                                    Text(
+                                        text = "GFG",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center,
+                                        color = Color.White
+                                    )
+                                })
+                        }) {
+                        contactPicker(
+                            context = LocalContext.current, contactName, contactNumber
+                        )
+                    }
                 }
             }
+        }
+
+        registerForActivityResult(ActivityResultContracts.GetContent()){contactData ->
+
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) return
+        if (requestCode == 1 && data != null) {
+            val contactData: Uri? = data.data
+            val cursor: Cursor = managedQuery(contactData, null, null, null, null)
+            cursor.moveToFirst()
+            val number: String =
+                cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))
+            val name: String =
+                cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+            contactName = name
+            contactNumber = number
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun contactPicker(
+    context: Context,
+    contactName: String,
+    contactNumber: String,
+) {
+    val activity = LocalContext.current as Activity
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Contact in Android",
+            color = Color.Green, fontSize = 20.sp, fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = contactName, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = contactNumber, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {
+                    requestContactPermission(context, activity)
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                    intent.type = ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE
+                    activity.startActivityForResult(intent, 1, null)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Text(text = "Pick Contact")
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Project_2Theme {
-        Greeting("Android")
+fun hasContactPermission(context: Context): Boolean {
+    return ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS) ==
+            PackageManager.PERMISSION_GRANTED;
+}
+fun requestContactPermission(context: Context, activity: Activity) {
+    if (!hasContactPermission(context)) {
+        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.READ_CONTACTS), 1)
     }
 }
--- mobile_dev/project_2/app/src/main/java/com/example/project_2/core/Contacts.kt
+++ mobile_dev/project_2/app/src/main/java/com/example/project_2/core/Contacts.kt
@@ -1,12 +1,20 @@
 package com.example.project_2.core
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun ContactsEndsWith(){
+    val context = LocalContext.current
+    context
     Column() {
-        Text("Hey")
+        Button(onClick = { /*TODO*/ }) {
+            Text("Hey")
+        }
     }
 }
