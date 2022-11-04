634af672d910bad09ca2d5b47b155eb2c171104d
Serhii <sergey.crisp@gmail.com>
2022-11-04 12:01:16 +0200

    init 2 mobile dev project

---
+++ mobile_dev/project_2/app/src/main/java/com/example/project_2/core/Contacts.kt
@@ -0,0 +1,12 @@
+package com.example.project_2.core
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ContactsEndsWith(){
+    Column() {
+        Text("Hey")
+    }
+}
