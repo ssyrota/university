67ee4050b487974a01f9ae071c802c1917233032
Serhii <sergey.crisp@gmail.com>
2022-11-05 13:52:13 +0200

    add maps

---
+++ mobile_dev/project_2/app/src/main/java/com/example/project_2/ui/MapPage.kt
@@ -0,0 +1,9 @@
+package com.example.project_2.ui
+
+import androidx.compose.runtime.Composable
+import com.google.maps.android.compose.GoogleMap
+
+@Composable
+fun MapPage() {
+    GoogleMap()
+}
--- mobile_dev/project_2/app/src/main/java/com/example/project_2/ui/Screen.kt
+++ mobile_dev/project_2/app/src/main/java/com/example/project_2/ui/Screen.kt
@@ -1,9 +1,6 @@
 package com.example.project_2.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
@@ -11,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -47,10 +45,13 @@ fun ComposeExamplesApp() {
             composable(Screen.Main.name) {
                 MainPage(
                     MainPageProps(
-                        mapsClick = {/*TODO*/ },
+                        mapsClick = { navController.navigate(Screen.Maps.name) },
                         contactsClick = {/*TODO*/ },
                         sqliteClick = {/*TODO*/ })
                 )
+            }
+            composable(Screen.Maps.name) {
+                MapPage()
             }
         }
 
@@ -75,6 +76,9 @@ fun TopBar(
                     overflow = TextOverflow.Ellipsis,
                     style = MaterialTheme.typography.headlineMedium
                 )
+                if (canBack) {
+                    Spacer(modifier = Modifier.padding(10.dp))
+                }
             }
         }, navigationIcon = {
             if (canBack) {
