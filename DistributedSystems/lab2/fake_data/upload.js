119538af4a5e4f5eb3a586af3d2cd09789e62827
Serhii <sergey.crisp@gmail.com>
2024-02-19 22:03:30 +0200

    _

--- DistributedSystems/lab2/fake_data/upload.js
+++ DistributedSystems/lab2/fake_data/upload.js
@@ -78,9 +78,76 @@ const uploadToPsql = async () => {
   }
 };
 
+const uploadToNeo4j = async () => {
+  const neo4j = require("neo4j-driver");
+  const fs = require("fs");
+  const uri = "bolt://neo4j:7687";
+  const driver = neo4j.driver(uri);
+  const session = driver.session();
+
+  const fileContents = fs.readFileSync("data.json", "utf8");
+  const users = JSON.parse(fileContents);
+
+  for (const user of users) {
+    // Create User node
+    await session.run(
+      `MERGE (u:User {login: $login, password: $password})`,
+      user
+    );
+
+    // Create CV node and relationship to User
+    await session.run(
+      `
+            MATCH (u:User {login: $login})
+            MERGE (c:CV {id: $cvId})
+            MERGE (u)-[:HAS_CV]->(c)
+        `,
+      { login: user.login, cvId: user.cv.id }
+    );
+
+    // Create and relate Hobbies
+    for (const hobby of user.cv.hobbies) {
+      await session.run(
+        `
+                MERGE (h:Hobby {id: $id, name: $name})
+                WITH h
+                MATCH (c:CV {id: $cvId})
+                MERGE (c)-[:HAS_HOBBY]->(h)
+            `,
+        { id: hobby.id, name: hobby.name, cvId: user.cv.id }
+      );
+    }
+
+    // Create and relate Jobs with Cities
+    for (const job of user.cv.jobs) {
+      await session.run(
+        `
+                MERGE (city:City {id: $cityId, name: $cityName})
+                MERGE (j:Job {id: $jobId})
+                ON CREATE SET j.from = $from, j.to = $to, j.company = $company
+                WITH j, city
+                MATCH (c:CV {id: $cvId})
+                MERGE (j)-[:LOCATED_IN]->(city)
+                MERGE (c)-[:HAS_JOB]->(j)
+            `,
+        {
+          cityId: job.city.id,
+          cityName: job.city.name,
+          jobId: job.id,
+          from: job.from,
+          to: job.to,
+          company: job.company,
+          cvId: user.cv.id,
+        }
+      );
+    }
+  }
+};
+
 async function main() {
   // await uploadToMongo();
   // await uploadToPsql();
+  await uploadToNeo4j();
 }
 
 main().catch(console.error);
