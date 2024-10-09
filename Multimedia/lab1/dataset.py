2cb514d65797fa79a9c5598e38140fa5120e7306
Serhii <sergey.crisp@gmail.com>
2024-10-09 22:29:27 +0300

    feat(multimedia): generate melgrams for dataset

--- Multimedia/lab1/dataset.py
+++ Multimedia/lab1/dataset.py
@@ -1,10 +1,30 @@
+from os import mkdir, path
 import pandas as pd
-import librosa
+from sound import Sound
 
-class UrbanSound:
-  def __init__(self, meta_path:str, audio_path:str):
+class UrbanSound8K:
+  def __init__(self, dataset_path:str, meta_path:str, audio_path:str):
     self.audio_path = audio_path
+    self.images_path = path.join(dataset_path, "images")
     self.data = pd.read_csv(meta_path)
 
   def generate_images(self):
-    librosa
+    self._ensure_image_folders()
+    
+    for i in range(len(self.data)):
+      row = self.data.iloc[i]
+      sound_file_path=path.join(self.audio_path, "fold"+str(row['fold']), row['slice_file_name'])
+      jpeg_path=path.join(self.images_path, "fold"+str(row['fold']), row['slice_file_name'])+'.jpg'
+      Sound(sound_file_path).export_melgram(jpeg_path)
+    
+  def _ensure_image_folders(self):
+    print("Creating image folders")
+    self._ensure_folder(self.images_path)
+    for i in range(1, 11):
+      self._ensure_folder(path.join(self.images_path, "fold"+str(i)))
+  
+  def _ensure_folder(self, folder):
+    try:
+      mkdir(folder)
+    except FileExistsError:
+      pass
--- Multimedia/lab1/sound.py
+++ Multimedia/lab1/sound.py
@@ -2,16 +2,31 @@ import librosa
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
+# memory leak fix
+import matplotlib
+matplotlib.use('Agg')
+
 
 class Sound:
     def __init__(self, path):
         self.sound_path = path
 
-    def export_merlgram(self, export_path):
+
+    def export_melgram(self, export_path):
+        MELS=128
         samples, sample_rate = librosa.load(self.sound_path, sr=None)
         sgram = librosa.stft(samples)
         sgram_mag, _ = librosa.magphase(sgram)
-        mel_scale_sgram = librosa.feature.melspectrogram(S=sgram_mag, sr=sample_rate)
+        mel_scale_sgram = librosa.feature.melspectrogram(S=sgram_mag, sr=sample_rate, n_mels=MELS)
         mel_sgram = librosa.amplitude_to_db(mel_scale_sgram, ref=np.min)
-        librosa.display.specshow(mel_sgram)
-        plt.savefig(export_path)
+        
+        # hack to make each mel have one pixel of height
+        fig, ax = plt.subplots(figsize=(256 / 100, 2*MELS / 100))
+        librosa.display.specshow(mel_sgram, ax=ax)
+        ax.axis('off')
+        plt.tight_layout(pad=0)
+        plt.savefig(export_path, format='jpg', bbox_inches='tight', pad_inches=0)
+        # memory leak fix
+        plt.close(fig)
+        fig.clf()
+        del fig, ax
